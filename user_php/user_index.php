4088e951d31b8c842e268a435b307785bfc25625
本松征也 <MotomatsuSeiya@Seiya-PC.local>
2018-09-29 17:48:04 +0900

    第8回課題

---
+++ user_php/user_index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>ログイン</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="user_select.php">データ一覧</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="insert.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>ログイン</legend>
+     <label>Name：<input type="text" name="name"></label><br>
+     <label>login ID：<input type="text" name="lid"></label><br>
+     <label>password：<input type="text" name="lpw"></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
